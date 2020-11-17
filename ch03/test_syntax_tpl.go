123a411bf58bca0ac82893a07c692be455d2f309
曾栋梁 <zengdongliang@i-yin.com.cn>
2020-11-17 22:15:23 +0800

    2020111702

--- ch03/test_syntax_tpl.go
+++ ch03/test_syntax_tpl.go
@@ -5,13 +5,21 @@ import (
 	"net/http"
 )
 
+type Article struct {
+	Id    int
+	Title string
+	Desc  string
+}
+
 func TestSyntaxTpl(ctx *gin.Context) {
 
 	name := "dlzeng"
 	arr := []int{1, 2, 3, 4, 5}
+	article := Article{Id: 1, Title: "西游记", Desc: "西游记外传,不容错过!"}
 	mapData := map[string]interface{}{
-		"name": name,
-		"arr":  arr,
+		"name":    name,
+		"arr":     arr,
+		"article": article,
 	}
 	ctx.HTML(http.StatusOK, "ch03/test.html", mapData)
 }
